12cdfd59f49b2138a3f69ddd151b9a05ea01ec07
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-14 21:31:18 +0800

    Create src\App.vue

---
+++ src\App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+      <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('./assets/css/release.css');
+@import url('./assets/css/common.css');
+
+</style>
