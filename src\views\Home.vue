5abbf63cfd0bdb4fd3600f89ae0ba487b6915a1e
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-14 21:29:24 +0800

    Create src\views\Home.vue

---
+++ src\views\Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="content">
+    <div>
+      <Aside ref="aside" />
+    </div>
+    <div class="main">
+      <Header ref="header" />
+      <div class="box">
+        <transition name="slide-right" mode="out-in">
+          <router-view class="child-view"></router-view>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Aside from '@/components/home/Aside.vue'
+import Header from '@/components/home/Header.vue'
+export default {
+  components: {
+    Aside,
+    Header
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  width: 100%;
+  display: flex;
+  background: #f1f5f8;
+}
+
+.main {
+  flex: 1;
+}
+
+.box {
+  height: calc(100vh - 60px);
+  box-sizing: border-box;
+  overflow-y: auto;
+  padding: 20px 20px 0 20px;
+}
+
+.child-view {
+  transition: all 0.3s;
+}
+.slide-right-leave-active {
+  opacity: 0;
+  // -webkit-transform: translate(100%, 0);
+  // transform: translate(100%, 0);
+}
+.slide-right-enter {
+  opacity: 0;
+  // -webkit-transform: translate(-100%, 0);
+  // transform: translate(-100%, 0);
+}
+</style>
