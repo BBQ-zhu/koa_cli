b4c9db80749fd2e621f0ae0471d6db8b971b192c
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:21:06 +0800

    Update src\store\index.js

--- src\store\index.js
+++ src\store\index.js
@@ -1,1 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+const {getNavMenus} = require("../router/navmenu")
+Vue.use(Vuex)
 
+export default new Vuex.Store({
+  state: {
+    userInfo: {},
+    oldNavMenu: [],
+    newNavMenu: []
+  },
+  mutations: {
+    storageUserInfo(state, e) {
+      e.token = "Bearer " + e.token
+      sessionStorage.setItem('userInfo', JSON.stringify(e))
+      state.userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+    }
+  },
+  actions: {
+    async getNav(contxt,router){
+      let userInfo = JSON.parse(sessionStorage.getItem('userInfo')||{})
+      await getNavMenus(userInfo,router)
+    }
+  },
+  modules: {}
+})
