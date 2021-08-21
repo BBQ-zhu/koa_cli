f78e9e72a81b24906f9b3a6c6bffa69e581b1a9b
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:20:12 +0800

    Create src\views\Contract.vue

---
+++ src\views\Contract.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">合同中心</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
