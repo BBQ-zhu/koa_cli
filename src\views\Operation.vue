fb61a9755bf84dc7378401b8ec5ae65eccac8bf6
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:17:56 +0800

    Create src\views\Operation.vue

---
+++ src\views\Operation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">操作记录</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
