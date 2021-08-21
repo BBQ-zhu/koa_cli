ceb9aeaef185e66c6b3ce1cd5318a0bc96d2f5a3
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:14:46 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,29 @@
 
+module.exports = {
+  devServer: {
+    open: true,
+    port: 8002,
+    https: false,
+    hotOnly: false,   
+    // http 代理配置
+    proxy: {      
+      '/api': {
+        target: 'http://localhost:3030',
+        changeOrigin: true,
+        // pathRewrite: {          
+        //   '^/api': '/api'
+        // }
+      },
+      '/upload/ueditor/image': {
+        target: 'http://localhost:3030',
+        changeOrigin: true
+      },
+      '/upload/ueditor/video': {
+        target: 'http://localhost:3030',
+        changeOrigin: true
+      }
+    },
+    before: (app) => {}
+  },
+  publicPath:''
+}
