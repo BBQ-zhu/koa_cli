ebf33fe804abce49a51faf7c428efe054efa1c4d
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-21 17:20:27 +0800

    Create src\views\Client.vue

---
+++ src\views\Client.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">前端用户管理</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
