1bcfffb705c1f35dc237e65a9fca6a5c8f49c3d5
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-08-27 17:18:16 +0800

    Update src\assets\js\common.js

--- src\assets\js\common.js
+++ src\assets\js\common.js
@@ -1,1 +1,32 @@
+//深克隆
+const clone = (obj) => {
+  let newObj = Array.isArray(obj) ? [] : {}
+  if (obj && typeof obj === "object") {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        newObj[key] = (obj && typeof obj[key] === 'object') ? clone(obj[key]) : obj[key];
+      }
+    }
+  }
+  return newObj
+}
 
+//下载文件
+const download = (src,filName)=>{
+  let x = new XMLHttpRequest();
+  x.open("GET",src,true);
+  x.responseType = "blob";
+  x.onload = function(e){
+    let url = window.URL.createObjectURL(x.response)
+    let a = document.createElement('a')
+    a.href= url
+    a.download = filName
+    a.click()
+  }
+  x.send()
+}
+
+module.exports = {
+  clone,
+  download
+}
