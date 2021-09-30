bf1ac30b626d736c59295ff4bcf9dbd9674c925c
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:43:43 +0800

    Update src\main.js

--- src\main.js
+++ src\main.js
@@ -7,8 +7,10 @@ import router from './router';
 import {get,post} from './request/http';
 import commonJS from './assets/js/common';
 import api from './request/api';
+import {createlogsImg} from './request/logs';
 import store from './store';
 import VueParticles from 'vue-particles' //动态粒子插件
+// import mixin from './components/mixins/mixin';
 // import 'vue-particle-line/dist/vue-particle-line.css'
 
 //Ueditor富文本编辑器引入
@@ -23,8 +25,48 @@ Vue.use(ElementUI);
 Vue.prototype.$axios = {get,post}
 Vue.prototype.$common = commonJS
 Vue.prototype.$api = api
+Vue.prototype.$logsImg = {createlogsImg}
 Vue.use(VueRouter);
 Vue.use(VueParticles)
+// Vue.mixin(mixin)
+
+
+// 全局混入
+
+Vue.mixin({
+  data(){
+    return{
+      meth:[false,false,false,false] // 增 删 改 查
+    }
+  },
+  methods:{
+    mixinMethod(path){
+      let nav = JSON.parse(sessionStorage.getItem('userPermission') || '{}').navList
+      this.meth = [false,false,false,false]
+      for(let i =0;i<nav.length;i++){
+        for(let j =0;j<nav[i].children.length;j++){
+          if(path == nav[i].children[j].path){
+            let val = nav[i].children[j].meth || []
+            val.map(item=>{
+              if(item == '新增'){
+                this.meth[0] = true
+              }else if(item == '删除'){
+                this.meth[1] = true
+              }else if(item == '修改'){
+                this.meth[2] = true
+              }else if(item == '查询'){
+                this.meth[3] = true
+              }
+            })
+          }
+        }
+      }
+      
+    }
+  }
+})
+
+
 
 
 new Vue({
@@ -35,5 +77,4 @@ new Vue({
     store.dispatch('getNav', router)
   },
 }).$mount('#app')
-
 
