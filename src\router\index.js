f2313519ce65ea570c5d0209f3ed6b0edb5e170a
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:36:13 +0800

    Update src\router\index.js

--- src\router\index.js
+++ src\router\index.js
@@ -1,10 +1,9 @@
-
 import Vue from "vue"
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
-import Login from "../views/Login.vue";
-// const Home = () => import("@/views/Home");
+// import Login from "../views/Login.vue";
+const Login = () => import("@/views/Login");
 const routes = [{
     path: '/',
     name:'登录',
@@ -17,9 +16,14 @@ const router = new VueRouter({
   routes //必须这样写routes
 })
 
-//全局路由导航守卫
+//全局路由导航守卫 前置守卫
 // router.beforeEach((to, from, next) => {
 //   next()
+// })
+
+//后置守卫
+// router.afterEach((to,from)=>{
+//   // console.log(to,from)
 // })
 
 export default router;
