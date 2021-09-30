9887c32d1a5b4b190e6e02e40c7d3f965b626c48
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-09-30 18:37:54 +0800

    Update src\views\Contract.vue

--- src\views\Contract.vue
+++ src\views\Contract.vue
@@ -2,9 +2,10 @@
   <div class="card">
     <div class="header flex">
       <el-button
+        v-if="meth[0]"
         type="primary"
         icon="el-icon-upload"
-        @click="(dialogVisible = true), (isAdd = true)"
+        @click="uploadNew"
       >发起签约</el-button>
       <el-select
         class="ml20"
@@ -62,19 +63,63 @@
             <el-form-item label="客户电话:" prop="phone">
               <el-input v-model="ruleForm.phone" placeholder="请输入客户电话"></el-input>
             </el-form-item>
+            <el-form-item label="服务费用:" prop="expenses">
+              <el-input v-model="ruleForm.expenses" placeholder="请输入服务费用"></el-input>
+            </el-form-item>
             <el-form-item label="付款方式:" prop="manner">
               <el-input v-model="ruleForm.manner" placeholder="请输入付款方式"></el-input>
             </el-form-item>
-            <el-form-item label="约定费用:" prop="expenses">
-              <el-input v-model="ruleForm.expenses" placeholder="请输入约定费用"></el-input>
+            <el-form-item label="签约提示:" prop="conname">
+              <el-input v-model="ruleForm.conname" placeholder="请输入签约提示"></el-input>
             </el-form-item>
-            <el-form-item label="合同名称:" prop="conname">
-              <el-input disabled v-model="ruleForm.conname" placeholder="请输入合同名称"></el-input>
+            <el-form-item label="起始日期:" prop="startime">
+              <el-date-picker style="width: 100%"
+                v-model="ruleForm.startime"
+                type="date"
+                value-format="yyyy-MM-dd"
+                placeholder="选择起始日期">
+              </el-date-picker>
             </el-form-item>
-            <el-form-item label="合同文档:" prop="imgurl">
+            <el-form-item label="结束日期:" prop="endtime">
+              <el-date-picker style="width: 100%"
+                v-model="ruleForm.endtime"
+                type="date"
+                value-format="yyyy-MM-dd"
+                placeholder="选择结束日期">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="提示方式:" prop="optertime">
+              <el-select style="width: 100%" v-model="ruleForm.optertime" placeholder="请选择提示方式">
+                <el-option label="月提示" value="月提示"></el-option>
+                <el-option label="年提示" value="年提示"></el-option>
+                <el-option label="不提示" value="不提示"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="提示日期:" prop="tipstime">
+              <el-input placeholder="请输入提前几天提示,例：7" v-model="ruleForm.tipstime"></el-input>
+            </el-form-item>
+            <el-form-item label="客户经理:" prop="manager1">
+              <el-input placeholder="请输入客户经理工号" v-model="ruleForm.manager1"></el-input>
+            </el-form-item>
+            <el-form-item label="权证经理:" prop="manager2">
+              <el-input placeholder="请输入权证经理工号" v-model="ruleForm.manager2"></el-input>
+            </el-form-item>
+            <el-form-item label="审核经理:" prop="manager2">
+              <el-input placeholder="请输入审核经理工号" v-model="ruleForm.manager3"></el-input>
+            </el-form-item>
+            <el-form-item label="签约状态:" prop="status">
+              <el-select style="width: 100%" v-model="ruleForm.status" placeholder="请选择签约状态" @change="statusChange">
+                <el-option label="待签约" value="待签约"></el-option>
+                <el-option label="审核中" value="审核中"></el-option>
+                <el-option label="驳回" value="驳回"></el-option>
+                <el-option label="签约成功" value="签约成功"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="合同图片:" prop="imgurl">
               <div style="position: relative">
-                <el-input placeholder="请上传合同文档" v-model="ruleForm.imgurl" disabled></el-input>
+                <el-input placeholder="请上传合同图片" v-model="ruleForm.imgurl" disabled></el-input>
                 <el-upload
+                  v-if="meth[0] || meth[2]"
                   style="position: absolute; top: 0; right: -110px"
                   class="upload-demo"
                   :data="ruleForm"
@@ -95,7 +140,7 @@
       </span>
       <div class="flex mt10 windBtn">
         <el-button @click="handleClose">取 消</el-button>
-        <el-button type="primary" @click="uploadBtn('ruleForm')">确 定</el-button>
+        <el-button type="primary" v-if="meth[0] || meth[2]" @click="uploadBtn('ruleForm')">确 定</el-button>
       </div>
     </div>
     <!-- 表格区域 -->
@@ -106,11 +151,10 @@
             <div slot="content">
               <img :src="scope.row.imgurl" />
             </div>
-            <!-- <img :src="scope.row.imgurl" class="tableImg" /> -->
             <span
-              class="tableImg"
-              @click="$common.download(scope.row.imgurl,scope.row.conname)"
-            >{{scope.row.conname}}</span>
+              class="colorBlue"
+              @click="$common.download(scope.row.imgurl,scope.row.name)"
+            >{{scope.row.name}}</span>
           </el-tooltip>
         </template>
       </el-table-column>
@@ -130,6 +174,7 @@
             size="small"
           >编辑</el-button>
           <el-button
+            v-if="meth[1]"
             @click.native.prevent="deleteRow(scope.$index, scope.row)"
             type="text"
             size="small"
@@ -161,7 +206,7 @@ export default {
   data() {
     return {
       uploadHeader: {
-        authorization: JSON.parse(sessionStorage.getItem('userInfo') || {})
+        authorization: JSON.parse(sessionStorage.getItem('userInfo') || '{}')
           .token
       },
       config: {
@@ -186,29 +231,36 @@ export default {
         imgurl: '',
         expenses: '', //费用
         manner: '', //付款方式
-        status: '', //签约状态 //初始，待签，待确认，签订完成
+        status: '待签约', //签约状态-待签约，待确认，驳回，签约成功
         remarks: '',
-        time: ''
+        startime:'',
+        endtime:'',
+        optertime:'',
+        tipstime:'7',
+        manager1: '', //客户经理
+        manager2: '', //权证经理
+        manager3: '', //审核经理
+        time: '' //创建时间
       },
       rules: {
         name: [{ required: true, message: '请输入产品名称', trigger: 'blur' }],
         type: [{ required: true, message: '请选择合同类型', trigger: 'blur' }],
         phone: [{ required: true, message: '请输入客户电话', trigger: 'blur' }],
-        expenses: [
-          { required: true, message: '请输入约定费用', trigger: 'blur' }
-        ],
-        imgurl: [
-          { required: true, message: '请上传合同扫描件', trigger: 'blur' }
-        ]
+        expenses: [{ required: true, message: '请输入约定费用', trigger: 'blur' }],
+        startime: [{ required: true, message: '请选择合同起始日期', trigger: 'blur' }],
+        endtime: [{ required: true, message: '请选择合同结束日期', trigger: 'blur' }],
+        optertime: [{ required: true, message: '请选择提示方式', trigger: 'blur' }]
       },
       tableData: [],
       tableHeader: [
         { name: '合同类型', prop: 'type' },
         { name: '签约状态', prop: 'status' },
-        { name: '客户名称', prop: 'name' },
+        { name: '签约提示', prop: 'conname' },
         { name: '客户电话', prop: 'phone' },
         { name: '约定费用', prop: 'expenses' },
-        { name: '付款方式', prop: 'manner' },
+        { name: '客户经理', prop: 'manager1' },
+        { name: '权证经理', prop: 'manager2' },
+        { name: '审核经理', prop: 'manager3' },
         { name: '创建时间', prop: 'time' }
       ],
       find: {
@@ -220,10 +272,38 @@ export default {
     }
   },
   mounted() {
+    this.mixinMethod(this.$route.path)
     this.getNewsList()
     this.findTagList()
   },
   methods: {
+    //上传按钮重置
+    uploadNew(){
+      this.dialogVisible = true
+      this.isAdd = true
+      this.ruleForm = {
+        type: '',
+        conname: '',
+        name: '',
+        phone: '',
+        imgurl: '',
+        expenses: '', //费用
+        manner: '', //付款方式
+        status: '待签约', //签约状态-待签约，待确认，驳回，签约成功
+        remarks: '',
+        startime:'',
+        endtime:'',
+        optertime:'',
+        tipstime:'7',
+        manager1: '', //客户经理
+        manager2: '', //权证经理
+        manager3: '', //审核经理
+        time: '' //创建时间
+      }
+    },
+    statusChange(e){
+      console.log(e)
+    },
     downLoad(url) {
       this.$common.download(url)
     },
@@ -235,13 +315,18 @@ export default {
         }
       })
     },
-    //上传产品图片成功钩子
-    handSuccess(res, file, fileList) {
+    //上传合同图片成功钩子
+    async handSuccess(res, file, fileList) {
       if (res.code == 200) {
+        if(!this.isAdd){
+          await this.$axios.post(this.$api.updataContract, {_id:this.ruleForm._id,imgurl: res.data})
+        }
+        if(this.ruleForm.imgurl){
+          await this.$axios.post(this.$api.delContractImg, this.ruleForm)
+        }
         this.ruleForm.imgurl = res.data
-        var fileName = res.data.split('/')
-        this.ruleForm.conname = fileName[fileName.length - 1]
         this.fileList = []
+        this.$logsImg.createlogsImg(this.$api.uploadContractImg,this.ruleForm.imgurl) //添加操作日志
         this.$message.success('文件上传成功')
       }
     },
@@ -283,10 +368,16 @@ export default {
       this.selectInput = ''
     },
     //编辑按钮
-    editRow(index, row) {
+    async editRow(index, row) {
       this.dialogVisible = true
       this.isAdd = false
-      this.ruleForm = row
+      await this.$axios.post(this.$api.findOneContract, {_id:row._id}).then(res => {
+        if(res.code == 200){
+          this.ruleForm = res.data[0]
+        }else{
+          this.$message.error('查询失败')
+        }
+      })
     },
     //删除行
     deleteRow(index, row) {
@@ -323,7 +414,7 @@ export default {
     async uploadBtn(formName) {
       this.$refs[formName].validate(async valid => {
         if (valid) {
-          this.ruleForm.details = this.$refs.editor.getUEContent()
+          this.ruleForm.remarks = this.$refs.editor.getUEContent()
           if (this.isAdd) {
             //新增
             delete this.ruleForm._id
@@ -332,6 +423,7 @@ export default {
               this.ruleForm
             )
           } else {
+            console.log(this.ruleForm)
             var res = await this.$axios.post(
               this.$api.updataContract,
               this.ruleForm
@@ -371,9 +463,6 @@ export default {
   width: 220px !important;
 }
 .tableImg:hover {
-  color: #409eff;
+  color: #8699ad;
 }
-/* .el-form-item{
-  width:302px!important;
-} */
 </style>
