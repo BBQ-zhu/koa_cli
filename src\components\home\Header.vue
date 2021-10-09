595db128b2637ee50b50b5f729b3987b8b5da4b2
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-09 19:39:47 +0800

    Update src\components\home\Header.vue

--- src\components\home\Header.vue
+++ src\components\home\Header.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="header bgColor">
-    <i v-if="isCollapse" class="el-icon-s-unfold f20 color0 pointer" @click="checkCollapse"></i>
-    <i v-else class="el-icon-s-fold f20 color0 pointer" @click="checkCollapse"></i>
+    <!-- <i class="el-icon-s-fold f20 color0 pointer"></i> -->
+    <i
+      v-if="isCollapse"
+      class="el-icon-s-unfold f20 color0 pointer"
+      @click="checkCollapse"
+    ></i>
+    <i
+      v-else
+      class="el-icon-s-fold f20 color0 pointer"
+      @click="checkCollapse"
+    ></i>
     <div class="header_right">
-      <div class="badge_item" style="line-height:60px;"><a href="#" class="f16">商城系统</a></div>
-      <div class="badge_item" style="line-height:60px;"><a href="#" class="f16">客服系统</a></div>
+      <div class="badge_item" style="line-height: 60px">
+        <a href="#" class="f16">商城管理系统</a>
+      </div>
+      <!-- <div class="badge_item" style="line-height: 60px">
+        <a href="#" class="f16">客服系统</a>
+      </div> -->
       <!-- 全屏 -->
       <i class="el-icon-full-screen badge_item pointer" @click="fullScree"></i>
       <!-- 警铃 -->
@@ -12,7 +25,12 @@
         <i class="el-icon-bell"></i>
       </el-badge>
       <!-- 头像 -->
-      <el-tooltip class="item pointer" effect="dark" :content="userInfo.role" placement="right">
+      <el-tooltip
+        class="item pointer"
+        effect="dark"
+        :content="userInfo.role"
+        placement="right"
+      >
         <div class="block mr20">
           <el-tooltip placement="bottom">
             <div slot="content">
@@ -25,7 +43,7 @@
       <!-- 下拉菜单 -->
       <el-dropdown @command="handleCommand">
         <span class="el-dropdown-link pointer">
-          {{userInfo.username}} | {{userInfo.uid}}
+          {{ userInfo.username }} | {{ userInfo.uid }}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
@@ -35,7 +53,12 @@
       </el-dropdown>
     </div>
 
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
       <span>
         <el-form
           :model="ruleForm"
@@ -46,91 +69,106 @@
           class="demo-ruleForm"
         >
           <el-form-item label="旧密码" prop="oldPass">
-            <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off"></el-input>
+            <el-input
+              type="password"
+              v-model="ruleForm.oldPass"
+              autocomplete="off"
+            ></el-input>
           </el-form-item>
           <el-form-item label="新密码" prop="pass">
-            <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+            <el-input
+              type="password"
+              v-model="ruleForm.pass"
+              autocomplete="off"
+            ></el-input>
           </el-form-item>
           <el-form-item label="确认密码" prop="checkPass">
-            <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+            <el-input
+              type="password"
+              v-model="ruleForm.checkPass"
+              autocomplete="off"
+            ></el-input>
           </el-form-item>
         </el-form>
       </span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >确 定</el-button
+        >
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import screenfull from 'screenfull' //全屏按钮F11效果
+import screenfull from "screenfull"; //全屏按钮F11效果
 export default {
   data() {
     var validateOldPass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入旧密码'))
+      if (value === "") {
+        callback(new Error("请输入旧密码"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     var validatePass = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请输入新密码'))
+      if (value === "") {
+        callback(new Error("请输入新密码"));
       } else {
-        if (this.ruleForm.checkPass !== '') {
-          this.$refs.ruleForm.validateField('checkPass')
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
         }
-        callback()
+        callback();
       }
-    }
+    };
     var validatePass2 = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error('请再次输入新密码'))
+      if (value === "") {
+        callback(new Error("请再次输入新密码"));
       } else if (value !== this.ruleForm.pass) {
-        callback(new Error('两次输入的新密码不一致!'))
+        callback(new Error("两次输入的新密码不一致!"));
       } else {
-        callback()
+        callback();
       }
-    }
+    };
     return {
       isCollapse: false,
-      userInfo: '',
+      userInfo: "",
       dialogVisible: false,
       ruleForm: {
-        oldPass: '',
-        pass: '',
-        checkPass: ''
+        oldPass: "",
+        pass: "",
+        checkPass: "",
       },
       rules: {
-        oldPass: [{ validator: validateOldPass, trigger: 'blur' }],
-        pass: [{ validator: validatePass, trigger: 'blur' }],
-        checkPass: [{ validator: validatePass2, trigger: 'blur' }]
-      }
-    }
+        oldPass: [{ validator: validateOldPass, trigger: "blur" }],
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+      },
+    };
   },
   created() {
-    this.userInfo = JSON.parse(sessionStorage.getItem('userInfo') || '{}')
+    this.userInfo = JSON.parse(sessionStorage.getItem("userInfo") || "{}");
   },
   methods: {
     fullScree() {
-      screenfull.toggle()
+      screenfull.toggle();
     },
     checkCollapse() {
-      this.isCollapse = !this.isCollapse
-      this.$parent.$refs.aside.checkCollapse()
+      this.isCollapse = !this.isCollapse;
+      this.$parent.$refs.aside.checkCollapse();
+      this.$parent.changeWidth()
     },
     handleCommand(command) {
       switch (command) {
-        case 'logout':
-          this.logout()
-          break
-        case 'changePwd':
-          this.changePwd()
-          break
+        case "logout":
+          this.logout();
+          break;
+        case "changePwd":
+          this.changePwd();
+          break;
         default:
-          break
+          break;
       }
     },
     async logout() {
@@ -138,51 +176,51 @@ export default {
       // this.$logsImg.createlogsImg(this.$api.uploadContractImg,this.ruleForm.imgurl) //添加操作日志
       let data = {
         user: `${this.userInfo.username}(${this.userInfo.uid})`,
-        logdata: '',
-        remarks: '退出登录'
-      }
-      await this.$axios.post(this.$api.createlogs, data) //创建日志
-      this.$router.push('/')
+        logdata: "",
+        remarks: "退出登录",
+      };
+      await this.$axios.post(this.$api.createlogs, data); //创建日志
+      this.$router.push("/");
     },
     //重置密码提交按钮
     submitForm(formName) {
-      this.$refs[formName].validate(valid => {
+      this.$refs[formName].validate((valid) => {
         if (valid) {
           var data = {
             uid: this.userInfo.uid,
             oldPassword: this.ruleForm.oldPass,
-            newPassword: this.ruleForm.pass
-          }
+            newPassword: this.ruleForm.pass,
+          };
           this.$axios
             .post(this.$api.updatePassword, data)
-            .then(res => {
+            .then((res) => {
               if (res.code == 200) {
                 this.$message({
-                  type: 'success',
-                  message: '修改密码成功'
-                })
+                  type: "success",
+                  message: "修改密码成功",
+                });
                 // this.handleClose()
-                this.logout()
+                this.logout();
               } else {
-                this.$message.error('修改密码失败')
+                this.$message.error("修改密码失败");
               }
             })
-            .catch(err => {
-              this.$message.error('原密码错误或服务器出错')
-            })
+            .catch((err) => {
+              this.$message.error("原密码错误或服务器出错");
+            });
         } else {
-          return false
+          return false;
         }
-      })
+      });
     },
     changePwd() {
-      this.dialogVisible = true
+      this.dialogVisible = true;
     },
     handleClose(done) {
-      this.dialogVisible = false
-    }
-  }
-}
+      this.dialogVisible = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
