3df8921bffbe1c416206eaf5fa3691d6db713b2e
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-09 19:41:55 +0800

    Update src\views\Operation.vue

--- src\views\Operation.vue
+++ src\views\Operation.vue
@@ -1,35 +1,43 @@
 <template>
   <div class="card">
     <div class="flex header">
-      <el-input placeholder="请输入内容" v-model="input" class="input-with-select findInput ">
+      <el-input
+        placeholder="请输入内容"
+        v-model="input"
+        class="input-with-select findInput"
+      >
         <el-select v-model="select" slot="prepend" placeholder="请选择">
           <el-option
-            v-for="(item,index) in tableHeader"
-            :key="index+'h'"
+            v-for="(item, index) in tableHeader"
+            :key="index + 'h'"
             :label="item.name"
             :value="item.prop"
           ></el-option>
         </el-select>
-        <el-button slot="append" icon="el-icon-search" @click="findFuzz()"></el-button>
+        <el-button
+          slot="append"
+          icon="el-icon-search"
+          @click="find.currentPage=1 ,findFuzz() "
+        ></el-button>
       </el-input>
     </div>
-      <el-table :data="tableData" stripe>
-        <el-table-column
-          show-overflow-tooltip
-          v-for="(item,index) in tableHeader"
-          :key="index+'1'"
-          :prop="item.prop"
-          :label="item.name"
-          min-width="100px"
-        ></el-table-column>
-      </el-table>
+    <el-table :data="tableData" stripe>
+      <el-table-column
+        show-overflow-tooltip
+        v-for="(item, index) in tableHeader"
+        :key="index + '1'"
+        :prop="item.prop"
+        :label="item.name"
+        min-width="100px"
+      ></el-table-column>
+    </el-table>
     <!-- 页码条 -->
     <el-pagination
       class="mt20"
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="find.currentPage"
-      :page-sizes="[10, 20, 50, 100,200]"
+      :page-sizes="[10, 15, 20, 50, 100, 200]"
       :page-size="10"
       layout="total, sizes, prev, pager, next, jumper"
       :total="find.total"
@@ -43,65 +51,54 @@ export default {
     return {
       tableData: [],
       tableHeader: [
-        { name: '操作人', prop: 'user' },
-        { name: '操作模块', prop: 'remarks' },
-        { name: '修改后的数据', prop: 'logdata' },
-        { name: '操作时间', prop: 'time' }
+        { name: "操作人", prop: "user" },
+        { name: "操作模块", prop: "remarks" },
+        { name: "修改后的数据", prop: "logdata" },
+        { name: "操作时间", prop: "time" },
       ],
-      input: '',
-      select: '',
+      input: "",
+      select: "",
       find: {
         currentPage: 1, //当前页码
         total: 0, //总页数
         skip: 0, //偏移量
-        limit: 10 //每一页的数量
+        limit: 10, //每一页的数量
       },
       ruleForm: {
-        user: '',
-        logdata:'',
-        remarks: '',
-        time:''
-      }
-    }
+        user: "",
+        logdata: "",
+        remarks: "",
+        time: "",
+      },
+    };
   },
   mounted() {
-    this.getLogsList()
+    this.findFuzz();
   },
   methods: {
-    //获取用户列表
-    getLogsList() {
-      var data = {
-        skip: this.find.limit * (this.find.currentPage - 1),
-        limit: this.find.limit
-      }
-      this.$axios.post(this.$api.findlogs, data).then(res => {
-        this.tableData = res.data[0].data
-        this.find.total = res.data[0].total[0].total
-      })
-    },
     //模糊查询
     findFuzz() {
       var data = {
         skip: this.find.limit * (this.find.currentPage - 1),
         limit: this.find.limit,
         fuzz: this.select,
-        input: this.input
-      }
-      this.$axios.post(this.$api.findlogs, data).then(res => {
-        this.tableData = res.data[0].data
-        this.find.total = res.data[0].total ? res.data[0].total[0].total : 0
-      })
+        input: this.input,
+      };
+      this.$axios.post(this.$api.findlogs, data).then((res) => {
+        this.tableData = res.data[0].data;
+        this.find.total = res.data[0].total ? res.data[0].total[0].total : 0;
+      });
     },
     handleSizeChange(val) {
-      this.find.limit = val
-      this.getLogsList()
+      this.find.limit = val;
+      this.findFuzz();
     },
     handleCurrentChange(val) {
-      this.find.currentPage = val
-      this.getLogsList()
-    }
-  }
-}
+      this.find.currentPage = val;
+      this.findFuzz();
+    },
+  },
+};
 </script>
 
 <style scoped>
