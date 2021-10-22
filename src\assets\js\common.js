92ba1ddd4dc8bdd815ffcf82e389866f0a50454d
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 16:15:35 +0800

    Update src\assets\js\common.js

--- src\assets\js\common.js
+++ src\assets\js\common.js
@@ -1,32 +1,48 @@
 //深克隆
 const clone = (obj) => {
-  let newObj = Array.isArray(obj) ? [] : {}
-  if (obj && typeof obj === "object") {
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        newObj[key] = (obj && typeof obj[key] === 'object') ? clone(obj[key]) : obj[key];
-      }
+    let newObj = Array.isArray(obj) ? [] : {}
+    if (obj && typeof obj === "object") {
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                newObj[key] = (obj && typeof obj[key] === 'object') ? clone(obj[key]) : obj[key];
+            }
+        }
     }
-  }
-  return newObj
+    return newObj
+}
+
+//冒泡排序：arr为数组对象，name为对象中需要排序的属性
+function bubbleSort(arr,name) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+        for (var j = 0; j < len - 1 - i; j++) {
+            if (parseInt(arr[j][name]) > parseInt(arr[j+1][name])) { // 相邻元素两两对比
+                var temp = arr[j+1]; // 元素交换
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 //下载文件
-const download = (src,filName)=>{
-  let x = new XMLHttpRequest();
-  x.open("GET",src,true);
-  x.responseType = "blob";
-  x.onload = function(e){
-    let url = window.URL.createObjectURL(x.response)
-    let a = document.createElement('a')
-    a.href= url
-    a.download = filName
-    a.click()
-  }
-  x.send()
+const download = (src, filName) => {
+    let x = new XMLHttpRequest();
+    x.open("GET", src, true);
+    x.responseType = "blob";
+    x.onload = function (e) {
+        let url = window.URL.createObjectURL(x.response)
+        let a = document.createElement('a')
+        a.href = url
+        a.download = filName
+        a.click()
+    }
+    x.send()
 }
 
 module.exports = {
-  clone,
-  download
+    clone,
+    download,
+    bubbleSort
 }
