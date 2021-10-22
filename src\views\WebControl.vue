76ce79effd9985397787099eb127db8a63b6d365
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 16:25:01 +0800

    Update src\views\WebControl.vue

--- src\views\WebControl.vue
+++ src\views\WebControl.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="card">
     <div class="header" v-if="meth[0]">
-      <el-button type="primary" icon="el-icon-upload" @click="dialogVisible=true,isAdd=true">上传新闻</el-button>
+      <el-button
+        type="primary"
+        icon="el-icon-upload"
+        @click="(dialogVisible = true), (isAdd = true)"
+        >上传新闻</el-button
+      >
     </div>
     <div v-if="dialogVisible" class="window">
       <span>
-        
         <el-form
           :model="ruleForm"
           :rules="rules"
@@ -15,27 +19,47 @@
         >
           <div class="">
             <el-form-item label="新闻标题:" prop="newsname">
-              <el-input v-model="ruleForm.newsname" placeholder="请输入新闻标题" style="width:200%"></el-input>
+              <el-input
+                v-model="ruleForm.newsname"
+                placeholder="请输入新闻标题"
+                style="width: 200%"
+              ></el-input>
             </el-form-item>
             <el-form-item label="新闻描述:" prop="message">
-              <el-input v-model="ruleForm.message" placeholder="请输入新闻描述" style="width:200%"></el-input>
+              <el-input
+                v-model="ruleForm.message"
+                placeholder="请输入新闻描述"
+                style="width: 200%"
+              ></el-input>
             </el-form-item>
             <el-form-item label="阅读量:" prop="num">
-              <el-input v-model="ruleForm.num" placeholder="请输入阅读量" style="width:200%"></el-input>
+              <el-input
+                v-model="ruleForm.num"
+                placeholder="请输入阅读量"
+                style="width: 200%"
+              ></el-input>
             </el-form-item>
           </div>
         </el-form>
         <div class="title mt20 mb20">新闻详情</div>
-        <UE :defaultMsg="ruleForm.content" :config="config" :id="ueId" ref="editor"></UE>
+        <UE
+          :defaultMsg="ruleForm.content"
+          :config="config"
+          :id="ueId"
+          ref="editor"
+        ></UE>
       </span>
       <div class="flex mt10 windBtn">
         <el-button @click="handleClose">取 消</el-button>
-        <el-button v-if="meth[2] || meth[0]" type="primary" @click="uploadAdd">确 定</el-button>
+        <el-button v-if="meth[2] || meth[0]" type="primary" @click="uploadAdd"
+          >确 定</el-button
+        >
       </div>
     </div>
     <el-table v-if="!dialogVisible" :data="tableData" stripe>
       <el-table-column
-        v-for="(item,index) in tableHeader"
+      sortable
+        v-for="(item, index) in tableHeader"
         :key="index"
         :prop="item.prop"
         :label="item.name"
@@ -48,13 +72,15 @@
             @click.native.prevent="editRow(scope.$index, scope.row)"
             type="text"
             size="small"
-          >编辑</el-button>
+            >编辑</el-button
+          >
           <el-button
             v-if="meth[1]"
             @click.native.prevent="deleteRow(scope.$index, scope.row)"
             type="text"
             size="small"
-          >删除</el-button>
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -65,7 +91,7 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="find.currentPage"
-      :page-sizes="[10, 20, 50, 100,200]"
+      :page-sizes="[10, 20, 50, 100, 200]"
       :page-size="10"
       layout="total, sizes, prev, pager, next, jumper"
       :total="find.total"
@@ -74,132 +100,136 @@
 </template>
 
 <script>
-import UE from '@/components/common/UE'
+import UE from "@/components/common/UE";
 export default {
   components: {
-    UE
+    UE,
   },
   data() {
     return {
-      content: '',
+      content: "",
       config: {
         initialFrameWidth: null,
-        initialFrameHeight: 220
+        initialFrameHeight: 220,
       },
-      ueId: 'editor1', //多实例情况下使用
+      ueId: "editor1", //多实例情况下使用
       dialogVisible: false,
       isAdd: true,
-      ruleForm:{
-        newsname:'',
-        message:'',
-        num:'',
-        creators:'',
-        content:'',
-        time:''
+      ruleForm: {
+        newsname: "",
+        message: "",
+        num: "",
+        creators: "",
+        content: "",
+        time: "",
       },
       rules: {
-        newsname: [{ required: true, message: '请输入新闻标题', trigger: 'blur' }],
-        message: [{ required: true, message: '请输入新闻描述', trigger: 'blur' }],
-        num: [{ required: true, message: '请输入阅读量', trigger: 'blur' }]
+        newsname: [
+          { required: true, message: "请输入新闻标题", trigger: "blur" },
+        ],
+        message: [
+          { required: true, message: "请输入新闻描述", trigger: "blur" },
+        ],
+        num: [{ required: true, message: "请输入阅读量", trigger: "blur" }],
       },
-      row: '',
+      row: "",
       tableData: [],
       tableHeader: [
-        { name: '新闻标题', prop: 'newsname' },
-        { name: '新闻描述', prop: 'message' },
-        { name: '阅读量', prop: 'num' },
-        { name: '发布者', prop: 'creators' },
-        { name: '创建时间', prop: 'time' }
+        { name: "新闻标题", prop: "newsname" },
+        { name: "新闻描述", prop: "message" },
+        { name: "阅读量", prop: "num" },
+        { name: "发布者", prop: "creators" },
+        { name: "创建时间", prop: "time" },
       ],
       find: {
         currentPage: 1, //当前页码
         total: 0, //总页数
         skip: 0, //偏移量
-        limit: 10 //每一页的数量
-      }
-    }
+        limit: 10, //每一页的数量
+      },
+    };
   },
   mounted() {
-    this.mixinMethod(this.$route.path)
-    console.log(this.meth)
-
-    this.getNewsList()
+    this.mixinMethod(this.$route.path);
+    console.log(this.meth);
+    this.getNewsList();
   },
   methods: {
     handleSizeChange(val) {
-      this.find.limit = val
-      this.getNewsList()
+      this.find.limit = val;
+      this.getNewsList();
     },
     handleCurrentChange(val) {
-      this.find.currentPage = val
-      this.getNewsList()
+      this.find.currentPage = val;
+      this.getNewsList();
     },
     async getNewsList() {
       var data = {
         skip: this.find.limit * (this.find.currentPage - 1),
-        limit: this.find.limit
-      }
-      this.$axios.post(this.$api.findNews, data).then(res => {
-        this.tableData = res.data[0].data
-        this.find.total = res.data[0].total[0].total
-      })
+        limit: this.find.limit,
+      };
+      this.$axios.post(this.$api.findNews, data).then((res) => {
+        this.tableData = res.data[0].data;
+        this.find.total = res.data[0].total[0].total;
+      });
     },
     //编辑按钮
     editRow(index, row) {
-      this.dialogVisible = true
-      this.isAdd = false
-      this.ruleForm = row
-      this.row = row
+      this.dialogVisible = true;
+      this.isAdd = false;
+      this.ruleForm = row;
+      this.row = row;
     },
     //删除行
     deleteRow(index, row) {
-      this.$confirm('确认删除该新闻吗？', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("确认删除该新闻吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
       }).then(() => {
-        this.$axios.post(this.$api.deleteNews, { _id: row._id }).then(res => {
+        this.$axios.post(this.$api.deleteNews, { _id: row._id }).then((res) => {
           if (res.code == 200) {
-            this.$message.success('删除成功')
-            this.getNewsList()
+            this.$message.success("删除成功");
+            this.getNewsList();
           }
-        })
-      })
+        });
+      });
     },
     handleClose() {
-      this.ruleForm = {
-        newsname:'',
-        message:'',
-        num:'',
-        creators:'',
-        content:'',
-        time:''
-      },
-      this.dialogVisible = false
+      (this.ruleForm = {
+        newsname: "",
+        message: "",
+        num: "",
+        creators: "",
+        content: "",
+        time: "",
+      }),
+        (this.dialogVisible = false);
     },
     async uploadAdd() {
       if (!this.ruleForm.newsname) {
-        this.$message.error('请先输入标题')
-        return
+        this.$message.error("请先输入标题");
+        return;
       }
-      this.ruleForm.content = this.$refs.editor.getUEContent()
+      this.ruleForm.content = this.$refs.editor.getUEContent();
       if (this.isAdd) {
         //新增
-        delete this.ruleForm._id
-        this.ruleForm.creators = JSON.parse(sessionStorage.getItem('userInfo') || '{}').username
-        var res = await this.$axios.post(this.$api.createNews, this.ruleForm)
+        delete this.ruleForm._id;
+        this.ruleForm.creators = JSON.parse(
+          sessionStorage.getItem("userInfo") || "{}"
+        ).username;
+        var res = await this.$axios.post(this.$api.createNews, this.ruleForm);
       } else {
-       var res = await this.$axios.post(this.$api.updateNews, this.ruleForm)
+        var res = await this.$axios.post(this.$api.updateNews, this.ruleForm);
       }
-
       if (res.code == 200) {
-        this.$message.success('操作成功')
-        this.dialogVisible = false
-        this.getNewsList()
+        this.$message.success("操作成功");
+        this.dialogVisible = false;
+        this.getNewsList();
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
