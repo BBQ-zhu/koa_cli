be79fae811d90c0be48af05fc26f225624a64a2e
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-11-13 20:33:27 +0800

    Update src\assets\js\common.js

--- src\assets\js\common.js
+++ src\assets\js\common.js
@@ -12,13 +12,13 @@ const clone = (obj) => {
 }
 
 //冒泡排序：arr为数组对象，name为对象中需要排序的属性
-function bubbleSort(arr,name) {
+function bubbleSort (arr, name) {
     var len = arr.length;
     for (var i = 0; i < len - 1; i++) {
         for (var j = 0; j < len - 1 - i; j++) {
-            if (parseInt(arr[j][name]) > parseInt(arr[j+1][name])) { // 相邻元素两两对比
-                var temp = arr[j+1]; // 元素交换
-                arr[j+1] = arr[j];
+            if (parseInt(arr[j][name]) > parseInt(arr[j + 1][name])) { // 相邻元素两两对比
+                var temp = arr[j + 1]; // 元素交换
+                arr[j + 1] = arr[j];
                 arr[j] = temp;
             }
         }
@@ -35,11 +35,26 @@ const download = (src, filName) => {
         let url = window.URL.createObjectURL(x.response)
         let a = document.createElement('a')
         a.href = url
+        console.log(url)
         a.download = filName
         a.click()
     }
     x.send()
 }
+
+//除去千分位
+
+function clearComma (num) {
+    if (num && num != 'undefined' && num != 'null') {
+        let numS = num;
+        numS = numS.toString();
+        numS = numS.replace(/,/gi, '');
+        return numS;
+    } else {
+        return num;
+    }
+}
+
 
 module.exports = {
     clone,
