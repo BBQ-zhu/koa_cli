9d4a19dcb2b1b3c2fcd143afa0ee7d9aee1b23e3
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-12-17 18:58:34 +0800

    Update src\assets\js\common.js

--- src\assets\js\common.js
+++ src\assets\js\common.js
@@ -11,8 +11,14 @@ const clone = (obj) => {
     return newObj
 }
 
+//号码中位数隐藏
+const phoneNum = (phone) => {
+    let arr = phone
+    return arr.slice(0, 3) + '****' + arr.slice(-4)
+}
+
 //冒泡排序：arr为数组对象，name为对象中需要排序的属性
-function bubbleSort (arr, name) {
+function bubbleSort(arr, name) {
     var len = arr.length;
     for (var i = 0; i < len - 1; i++) {
         for (var j = 0; j < len - 1 - i; j++) {
@@ -44,7 +50,7 @@ const download = (src, filName) => {
 
 //除去千分位
 
-function clearComma (num) {
+function clearComma(num) {
     if (num && num != 'undefined' && num != 'null') {
         let numS = num;
         numS = numS.toString();
@@ -55,9 +61,39 @@ function clearComma (num) {
     }
 }
 
+//依据数组对象中的某个值排序 // arr.sort(compare('age'))
+function compare(property){
+    return function(a,b){
+        var value1 = a[property];
+        var value2 = b[property];
+        return value1 - value2;
+    }
+}
+
+// 千位分隔符
+const moneyFormat = (val, none = '') => {
+    if (val === undefined || val === null || val === '') {
+        return none
+    } else {
+        // val = (val * 1).toFixed(2).toString()
+        val = (val * 1).toString()
+        const reg = /(?=(\B\d{3})+$)/g
+        if (val.indexOf('.') > -1) {
+            const index = val.indexOf('.')
+            const xiaoshu = val.slice(index, val.length)
+            const zhengshu = val.slice(0, index)
+            return zhengshu.replace(reg, ',') + xiaoshu
+        } else {
+            return val.replace(reg, ',')
+        }
+    }
+}
 
 module.exports = {
     clone,
     download,
-    bubbleSort
+    bubbleSort,
+    phoneNum,
+    compare,
+    moneyFormat
 }
